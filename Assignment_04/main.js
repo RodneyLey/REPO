6d0798b3bdbe951bf4b4e9936a803631c153271a
RodneyLey <rodneymwalr@gmail.com>
2023-12-14 03:39:07 -0400

    change the css

--- Assignment_04/main.js
+++ Assignment_04/main.js
@@ -1,49 +1,55 @@
-// Wait for the DOM to be fully loaded before executing the code
 document.addEventListener('DOMContentLoaded', function () {
-  // Get references to HTML elements
   const getDataButton = document.getElementById('getDataButton');
   const provinceInput = document.getElementById('provinceInput');
   const resultContainer = document.getElementById('resultContainer');
   const getAllProvincesLink = document.getElementById('getAllProvincesLink');
 
-  // Event listener for the "Get Data" button
   getDataButton.addEventListener('click', function () {
-      // Get the trimmed value of the province input
       const provinceName = provinceInput.value.trim();
 
       if (provinceName) {
           // Call the API to get information for a specific province
-          fetch(`https://aqpotz9zm3.execute-api.ca-central-1.amazonaws.com/provinces/${provinceName}`)
+          fetch(`https://aqpotz9zm3.execute-api.ca-central-1.amazonaws.com/provinces/${encodeURIComponent(provinceName)}`)
               .then(response => response.json())
               .then(data => {
                   // Display the result in the result container
-                  resultContainer.textContent = JSON.stringify(data, null, 2);
+                  resultContainer.innerHTML = `
+                      <h3>${data.name}</h3>
+                      <p>Capital: ${data.capital}</p>
+                      <p>Population: ${data.population}</p>
+                      <p>Random Fact: ${data.randomFact}</p>
+                      <img src="${data.image}" alt="${data.name}">
+                  `;
               })
               .catch(error => {
                   console.error('Error:', error);
                   resultContainer.textContent = 'An error occurred. Please try again.';
               });
       } else {
-          // Show an alert if the province name is empty
           alert('Please enter a province name.');
       }
   });
 
-  // Event listener for the "Get All Provinces" link
   getAllProvincesLink.addEventListener('click', function (event) {
-      // Prevent the default link behavior
       event.preventDefault();
 
-      // Call the API to get information for all provinces
       fetch('https://aqpotz9zm3.execute-api.ca-central-1.amazonaws.com/provinces')
           .then(response => response.json())
           .then(data => {
-              // Display the result in the result container
-              resultContainer.textContent = JSON.stringify(data, null, 2);
+              resultContainer.innerHTML = `
+                  <h2>All Provinces</h2>
+                  <ul>
+                      ${data.provinces.map(province => `<li>${province.name}</li>`).join('')}
+                  </ul>
+              `;
           })
           .catch(error => {
               console.error('Error:', error);
               resultContainer.textContent = 'An error occurred. Please try again.';
           });
+          .catch(error => {
+            console.error('Fetch Error:', error);
+            resultContainer.textContent = `An error occurred: ${error.message}`;
+        });
   });
 });
